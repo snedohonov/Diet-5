d9ea63f4ccf6a37db3aa03d32fa28698204aaf57
snedohonov <dnepr.dp1984@gmail.com>
2018-11-22 01:46:35 +0200

    Create search
    create searching functionality

--- Diet№5/ArrayViewController.swift
+++ Diet№5/ArrayViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchResultsUpdating {
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var searchBar: UISearchBar!
     
-
+    var filteredProducts = [Product]()
+    var searchController = UISearchController(searchResultsController: nil)
+    var resultsController = UISearchController()
     
-    
-    
-    var products: [Product] = [
+    var products = [
         Product(name: "Говядина (не жирная)", type: "мясо", allow: "allow", disallow: "disallow"),
         Product(name: "Телятина", type: "мясо", allow: "allow", disallow: "disallow"),
         Product(name: "Кролик (без шкуры)", type: "мясо", allow: "allow", disallow: "disallow"),
@@ -68,27 +67,78 @@ class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering() {
+            return filteredProducts.count
+        }
+        
         return products.count
     }
     
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ProductTableViewCell
     
-        cell.allowImage.image = UIImage(named: products[indexPath.row].allow)
-        cell.disallowImage.image = UIImage(named: products[indexPath.row].disallow)
-        cell.nameProductLabel.text = products[indexPath.row].name
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ProductTableViewCell
+        let product: Product
+        if isFiltering() {
+            product = filteredProducts[indexPath.row]
+        } else {
+            product = products[indexPath.row]
+        }
+        
+        
+        cell.allowImage.image = UIImage(named: product.allow)
+        cell.disallowImage.image = UIImage(named: product.disallow)
+        cell.nameProductLabel.text = product.name
         
         return cell
     }
     
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredProducts = products.filter({( products : Product) -> Bool in
+            return products.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+    
+    
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
     
         super.viewDidLoad()
-
         
-    
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск продукта"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+        
+        
+
         
     }
 }
